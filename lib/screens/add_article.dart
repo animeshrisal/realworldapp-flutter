81d21e4afba987a3f22f2c8cc1e2863e8959a109
animeshrisal99 <animesh@fawesomeapps.com>
2019-04-07 19:54:43 +0545

    create add article page

---
+++ lib/screens/add_article.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddArticle extends StatefulWidget {
+  _AddArticleState createState() => _AddArticleState();
+}
+
+class _AddArticleState extends State<AddArticle> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("AAAA");
+  }
+}
