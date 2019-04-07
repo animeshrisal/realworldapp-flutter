7c6a0b88b691ac62bf4090c441844ab6d6a755b2
Animesh Risal <animeshrisal99@gmail.com>
2019-04-07 21:28:53 +0545

    add adapter

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:realworldapp/screens/register.dart';
 import 'package:realworldapp/screens/home.dart';
 import 'package:realworldapp/screens/login.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+import 'package:jaguar_query_sqflite/jaguar_query_sqflite.dart';
+import 'package:realworldapp/models/auth.dart';
 
-void main() => runApp(MyApp());
+SqfliteAdapter _adapter;
+
+void main() async {
+  String dbPath = await getDatabasesPath();
+  _adapter = SqfliteAdapter(path.join(dbPath, "test.db"));
+
+  try {
+    await _adapter.connect();
+    final authBean = AuthBean(_adapter);
+
+    await authBean.createTable(ifNotExists: true);
+
+    _adapter.close();
+  } catch (e) {
+    print(e);
+  }
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
