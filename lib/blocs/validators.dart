f0db4754e58192d2de99dc939d8bd116156f1e6d
animeshrisal99 <animesh@fawesomeapps.com>
2019-04-09 14:46:15 +0545

    add validators

--- lib/blocs/validators.dart
+++ lib/blocs/validators.dart
@@ -27,4 +27,31 @@ class Validators {
       sink.addError('Invalid password, please enter more than 4 characters');
     }
   });
+
+  final validateTitle =
+      StreamTransformer<String, String>.fromHandlers(handleData: (title, sink) {
+    if (title.length > 0) {
+      sink.add(title);
+    } else {
+      sink.addError("Title can't be empty");
+    }
+  });
+
+  final validateBody =
+      StreamTransformer<String, String>.fromHandlers(handleData: (body, sink) {
+    if (body.length > 0) {
+      sink.add(body);
+    } else {
+      sink.addError("Body can't be empty");
+    }
+  });
+
+  final validateDescription = StreamTransformer<String, String>.fromHandlers(
+      handleData: (description, sink) {
+    if (description.length > 0) {
+      sink.add(description);
+    } else {
+      sink.addError("Description can't be empty");
+    }
+  });
 }
