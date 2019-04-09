00aec1cf4d4d42eedabb6f9f78b77cf54d21f786
animeshrisal99 <animesh@fawesomeapps.com>
2019-04-09 14:48:24 +0545

    add modelname

--- lib/blocs/network/network_event.dart
+++ lib/blocs/network/network_event.dart
@@ -2,10 +2,11 @@ import 'package:realworldapp/bloc_helpers/bloc_event_state.dart';
 import 'package:flutter/material.dart';
 
 class NetworkEvent<T> extends BlocEvent {
-  NetworkEvent({this.event, this.model});
+  NetworkEvent({this.event, @required this.model, @required this.modelName});
 
   final NetworkEventType event;
   final T model;
+  final String modelName;
 }
 
 enum NetworkEventType { request, success, failure }
