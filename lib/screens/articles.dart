2b92eeb287bdffa817821ad9f586b063f1320cf7
animeshrisal99 <animesh@fawesomeapps.com>
2019-04-09 14:47:32 +0545

    add override

--- lib/screens/articles.dart
+++ lib/screens/articles.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:realworldapp/blocs/article/article_form_bloc.dart';
+import 'package:realworldapp/blocs/authentication/authentication_bloc.dart';
+import 'package:realworldapp/bloc_helpers/bloc_provider.dart';
+import 'package:realworldapp/blocs/network/network_bloc.dart';
 
 class Articles extends StatefulWidget {
+  @override
   _ArticlesState createState() => _ArticlesState();
 }
 
 class _ArticlesState extends State<Articles> {
   @override
   Widget build(BuildContext context) {
-    return Text("Articles");
+    return Text("AAA");
   }
 }
