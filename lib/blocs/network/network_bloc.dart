4c0c9c5d14f7ca065592f049dc6dc1ca86022f1e
animeshrisal99 <animesh@fawesomeapps.com>
2019-04-09 14:48:17 +0545

    made generic network bloc

--- lib/blocs/network/network_bloc.dart
+++ lib/blocs/network/network_bloc.dart
@@ -2,20 +2,31 @@ import 'package:realworldapp/bloc_helpers/bloc_event_state.dart';
 import 'package:realworldapp/blocs/network/network_event.dart';
 import 'package:realworldapp/blocs/network/network_state.dart';
 import 'package:dio/dio.dart';
+import 'package:realworldapp/blocs/authentication/authentication_bloc.dart';
 
 class NetworkBloc<T> extends BlocEventStateBase<NetworkEvent, NetworkState> {
-  final String _endPoint = "http://192.168.31.55:4000/";
+  final String _endPoint = "http://192.168.31.55:4000/articles";
   final Dio _dio = Dio();
   final T bloc;
+  final AuthenticationBloc authenticationBloc;
 
-  NetworkBloc({this.bloc}) : super(initialState: NetworkState.initialization());
+  NetworkBloc({this.bloc, this.authenticationBloc})
+      : super(initialState: NetworkState.initialization());
 
   @override
   Stream<NetworkState> eventHandler(
       NetworkEvent event, NetworkState currentState) async* {
+    String token = authenticationBloc.lastState.jwt;
     if (event.event == NetworkEventType.request) {
-      Response response =
-          await _dio.post(_endPoint, data: {event.model.model: {}});
+      try {
+        Response response = await _dio.post(_endPoint,
+            data: {event.modelName: event.model},
+            options: Options(headers: {'Authorization': 'Token $token'}));
+
+        print(response.data);
+      } catch (e) {
+        print(e);
+      }
     }
   }
 }
