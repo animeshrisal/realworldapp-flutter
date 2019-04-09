282b84ce5566bb41152a6df69af82e4f320cbf20
Animesh Risal <animeshrisal99@gmail.com>
2019-04-09 16:52:27 +0545

    initialize article

--- lib/screens/articles.dart
+++ lib/screens/articles.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:realworldapp/blocs/network/network_bloc.dart';
+import 'package:realworldapp/blocs/network/network_event.dart';
 
 class Articles extends StatefulWidget {
   @override
@@ -6,6 +8,16 @@ class Articles extends StatefulWidget {
 }
 
 class _ArticlesState extends State<Articles> {
+  NetworkBloc networkBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    networkBloc = NetworkBloc();
+    networkBloc.emitEvent(
+        NetworkEvent(event: NetworkEventType.requestGet, url: '/articles'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text("Articles");
